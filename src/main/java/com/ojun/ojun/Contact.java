72447162c57d7ea68152c7d113e4d2ffc9b5236b
Roy Rafles Matorang Pasaribu <90742736+royraflesmatorangpasaribu@users.noreply.github.com>
2023-05-05 22:41:04 +0700

    Add files via upload

---
+++ src/main/java/com/ojun/ojun/Contact.java
@@ -0,0 +1,31 @@
+package com.ojun.ojun;
+
+public class Contact {
+    private String judul, name, photo;
+
+
+    public String getJudul() {
+        return judul;
+    }
+
+    public void setJudul(String judul) {
+        this.judul = judul;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+}
+
